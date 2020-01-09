b7af898f45ccf29a947eda0d16990a4a61ab870b
sukmabrahmantya <brahmantyasukma@gmail.com>
2020-01-09 13:35:25 +0700

    adding user model schema in server side

--- server/helpers/randomPassword.js
+++ server/helpers/randomPassword.js
@@ -1,3 +1,5 @@
+'use strict'
+
 module.exports = () => {
   const length = 8
   const chartset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789*@#$%&+="
---
+++ server/models/user.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const { Schema, model } = require('mongoose')
+const bcrypt = require('bcryptjs')
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: [true, 'Email cannot be empty'],
+    unique: [true, 'Email already taken']
+  },
+  password: {
+    type: String,
+    required: [true, 'Password cannot be empty'],
+    minlength: [8, 'Password must be 8 - 12 character'],
+    maxlength: [12, 'Passweod must be 8 - 12 characters']
+  }
+})
+
+userSchema.pre('save', function(next) {
+  const salt = bcrypt.genSaltSync(10)
+  this.password = bcrypt.hashSync(this.password, salt)
+  next()
+})
+
+const User = model('User', userSchema)
+
+module.exports = User
