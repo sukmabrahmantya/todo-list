387bba8869d7e76b218214158dcea83f0d0ef30a
sukmabrahmantya <brahmantyasukma@gmail.com>
2020-01-09 13:24:26 +0700

    addiing random passwor in helpers server side

---
+++ server/helpers/randomPassword.js
@@ -0,0 +1,11 @@
+module.exports = () => {
+  const length = 8
+  const chartset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789*@#$%&+="
+  let password = ""
+
+  for (let i = 0, n = chartset.length; i < length; i++) {
+    password += chartset.charAt(Math.floor(Math.random() * n))
+  }
+
+  return password
+}
