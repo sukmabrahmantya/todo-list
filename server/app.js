f70213597a1ed6049e8cbe27627ea0686ea85957
sukmabrahmantya <brahmantyasukma@gmail.com>
2020-01-09 13:11:11 +0700

    updated structure in app.js

---
+++ server/app.js
@@ -0,0 +1,24 @@
+'use strict'
+
+if(process.env.NODE_ENV === 'development') require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3001
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGO_LOCAL, {
+  useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true
+}, function(err) {
+  if(err) console.log(`Databas is an error`)
+  else console.log(`Database in an active`)
+})
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.listen(PORT, () => {
+  console.log(`Listening on PORT ${PORT}`)
+})
